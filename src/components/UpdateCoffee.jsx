e1ae0a636511c6ee488333201cc601bed892d09e
Programming Hero <programming.hero1@gmail.com>
2025-04-29 13:21:41 -0500

    Fetch coffee for update and created update form

--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,9 +1,57 @@
 import React from 'react';
+import { useLoaderData } from 'react-router';
 
 const UpdateCoffee = () => {
+    const {name, quantity, price, taste, supplier, photo, details} = useLoaderData();
+
+    const handleUpdateCoffee = e =>{
+        e.preventDefault();
+        const form = e.target;
+        const formData = new FormData(form);
+        const updatedCoffee = Object.fromEntries(formData.entries())
+        console.log(updatedCoffee);
+
+    }
+
     return (
-        <div>
-            <h2>Update Coffee</h2>
+        <div className='p-24'>
+            <div className='p-12 text-center space-y-4'>
+                <h1 className="text-6xl">Update Coffee</h1>
+            </div>
+            <form onSubmit={handleUpdateCoffee}>
+                <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label">Name</label>
+                        <input type="text" name='name' defaultValue={name} className="input w-full" placeholder="Coffee Name" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label">Quantity</label>
+                        <input type="text" name='quantity' defaultValue={quantity} className="input w-full" placeholder="Quantity Name" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label">Supplier</label>
+                        <input type="text" name='supplier' defaultValue={supplier} className="input w-full" placeholder="Supplier Name" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label">Taste</label>
+                        <input type="text" name='taste' defaultValue={taste} className="input w-full" placeholder="Taste Name" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label">Price</label>
+                        <input type="text" name='price' defaultValue={price} className="input w-full" placeholder="Price per Cup" />
+                    </fieldset>
+                    <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border p-4">
+                        <label className="label">Details</label>
+                        <input type="text" name='details' defaultValue={details} className="input w-full" placeholder="Details Name" />
+                    </fieldset>
+                </div>
+                <fieldset className="fieldset bg-base-200 border-base-300 rounded-box border my-6 p-4">
+                    <label className="label">Photo</label>
+                    <input type="text" name='photo' defaultValue={photo} className="input w-full" placeholder="Photo URL" />
+                </fieldset>
+
+                <input type="submit" className='btn w-full' value="Update Coffee" />
+            </form>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -33,6 +33,7 @@ const router = createBrowserRouter([
       },
       {
         path: 'updateCoffee/:id',
+        loader: ({params}) => fetch(`http://localhost:3000/coffees/${params.id}`) ,
         Component: UpdateCoffee
       }
     ]
